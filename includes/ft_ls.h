9a27210bc8bcb556f2130810fb4085f09e0432df
Robin Schramm <wobula@gmail.com>
2018-12-11 19:33:55 -0800

    have all file info in a data structure.

--- includes/ft_ls.h
+++ includes/ft_ls.h
@@ -2,6 +2,7 @@
 #include <dirent.h>
 #include <pwd.h>
 #include <grp.h>
+#include <time.h>
 #include "../libft/includes/libft.h"
 
 #define OPT_CHECK(x)\
@@ -12,7 +13,7 @@
 				 (x) == 'l' ? true :\
 				 (x) == 't' ? true : false\
 
-#define FILE_CHECK(x)\
+#define TYPE_CHECK(x)\
 				 (x) == S_IFDIR ? 'd' :\
 				 (x) == S_IFREG ? '-' :\
 				 (x) == S_IFLNK ? 'l' :\
@@ -24,19 +25,27 @@
 				 	ft_printf("ls invalid option -- '%c'\n", x);\
 				 	ft_printf("Try 'ls --dumbass' for more information.\n");\
 
-typedef struct 	s_args 
+typedef struct 		s_args 
 {
-	int 		argc;
-	int			opt_count;
-	int			arg_count;
-	char** 		argv;
-	char**		args;
-	bool		opts[127];
-} 				t_args;
+	int 			argc;
+	int				opt_count;
+	int				arg_count;
+	char** 			argv;
+	char**			args;
+	bool			opts[127];
+} 					t_args;
 
-typedef struct 	s_file
+typedef struct 		s_file
 {
-	char		protection[11];
-}				t_file;
+	char			protection[11];
+	char			*user;
+	char			*group;
+	char			*last_mod_eng;
+	uid_t			user_id;
+	gid_t			group_id;
+	off_t			size;
+	nlink_t			hlinks;
+	time_t			last_modified;
+}					t_file;
 
 bool 	preprocessor(t_args *meta, char **argv, int argc);
--- src/ls.c
+++ src/ls.c
@@ -1,116 +1,77 @@
 #include "../includes/ft_ls.h"
 
-// int lstat(const char *path, struct stat *buff); -- file info
-//struct stat {
-//    dev_t     st_dev;     /* ID of device containing file */
-//    ino_t     st_ino;     /* inode number */
-//    mode_t    st_mode;    /* protection */
-//    nlink_t   st_nlink;   /* number of hard links */
-//    uid_t     st_uid;     /* user ID of owner */
-//    gid_t     st_gid;     /* group ID of owner */
-//    dev_t     st_rdev;    /* device ID (if special file) */
-//    off_t     st_size;    /* total size, in bytes */
-//    blksize_t st_blksize; /* blocksize for file system I/O */
-//    blkcnt_t  st_blocks;  /* number of 512B blocks allocated */
-//    time_t    st_atime;   /* time of last access */
-//    time_t    st_mtime;   /* time of last modification */
-//    time_t    st_ctime;   /* time of last status change */
-//};
-
-/*S_ISREG(m)
-is it a regular file?
-S_ISDIR(m)
-directory?
-S_ISCHR(m)
-character device?
-S_ISBLK(m)
-block device?
-S_ISFIFO(m)
-FIFO (named pipe)?
-S_ISLNK(m)
-symbolic link? (Not in POSIX.1-1996.)
-S_ISSOCK(m)
-socket? (Not in POSIX.1-1996.)*/
-
-/*struct stat sb;
-
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <time.h>
-#include <stdio.h>
-#include <stdlib.h>
-
-if (argc != 2) {
-fprintf(stderr, "Usage: %s <pathname>\n", argv[0]);
-exit(EXIT_FAILURE);
-}
-
-if (stat(argv[1], &sb) == -1) {
-perror("stat");
-exit(EXIT_FAILURE);
-}*/
-
-void	print_type(t_file *data, mode_t st_mode)
+void	get_perms(t_file *data, mode_t st_mode)
 {
 	int bits;
 
 	bits = (st_mode & S_IFMT);
-	data->protection[0] = FILE_CHECK(bits);
-}
-
-void	print_perms(t_file *data, mode_t st_mode)
-{
-	print_type(data, st_mode);
-
-	//user
-    data->protection[1] = st_mode & S_IRUSR ? 'r' : '-';
+	data->protection[0] = TYPE_CHECK(bits);
+	data->protection[1] = st_mode & S_IRUSR ? 'r' : '-';
     data->protection[2] = st_mode & S_IWUSR ? 'w' : '-';
     data->protection[3] = st_mode & S_IXUSR ? 'x' : '-';
-
-    //group
     data->protection[4] = st_mode & S_IRGRP ? 'r' : '-';
     data->protection[5] = st_mode & S_IWGRP ? 'w' : '-';
     data->protection[6] = st_mode & S_IXGRP ? 'x' : '-';
-
-    //wtf?
     data->protection[7] = st_mode & S_IROTH ? 'r' : '-';
     data->protection[8] = st_mode & S_IWOTH ? 'w' : '-';
     data->protection[9] = st_mode & S_IXOTH ? 'x' : '-';
     data->protection[10] = '\0';
+}
 
-    ft_printf("perms: %s\n", data->protection);
+void	get_time(t_file *data, time_t last_mod)
+{
+	data->last_mod_eng = ctime(&last_mod);
+}
+
+void	get_user_details(t_file *data, uid_t user_id, gid_t group_id)
+{
+	struct passwd 	*pwd;
+	struct group 	*gwd;
+
+	pwd = getpwuid(user_id);
+	gwd = getgrgid(group_id);
+	data->user = pwd->pw_name;
+	data->group = gwd->gr_name;
+}
+
+void	get_size(t_file *data, off_t size)
+{
+	data->size = size;
+}
+
+void	get_links(t_file *data, nlink_t hlinks)
+{
+	data->hlinks = (long long)hlinks;
+}
+
+void	print_file(t_file *data)
+{
+	ft_printf("%s ", data->protection);
+	ft_printf("%s ", data->user);
+    ft_printf("%s ", data->group);
+    ft_printf("%lld ", (long long)data->size);
+    ft_printf("%lld ", (long long)data->hlinks);
+    ft_printf("%.12s", data->last_mod_eng + 4);
 }
 
 void	get_file_info(t_args *meta)
 {
 	struct stat sb;
-	struct passwd *pwd;
-	struct group *gwd;
 	t_file data;
-	int x;
 
-	if ((x = lstat(meta->args[0], &sb)) == -1)
+	if (lstat(meta->args[0], &sb) == -1)
 	{
 		ft_printf("Error");
 		return ;
 	}
-		ft_printf("return: %d\n", x);
-		switch (sb.st_mode & S_IFMT) {
-	    case S_IFBLK:  printf("block device\n");            break;
-	    case S_IFCHR:  printf("character device\n");        break;
-	    case S_IFDIR:  printf("directory\n");               break;
-	    case S_IFIFO:  printf("FIFO/pipe\n");               break;
-	    case S_IFLNK:  printf("symlink\n");                 break;
-	    case S_IFREG:  printf("regular file\n");            break;
-	    case S_IFSOCK: printf("socket\n");                  break;
-	    default:       printf("unknown?\n");                break;
-    }
-    pwd = getpwuid(sb.st_uid);
-    gwd = getgrgid(pwd->pw_gid);
-   	ft_printf("user: %s\n", pwd->pw_name);
-    ft_printf("group: %s\n", gwd->gr_name);
-    ft_printf("size: %jd\n", sb.st_size);
-    print_perms(&data, sb.st_mode);
+
+    get_perms(&data, sb.st_mode);
+    get_time(&data, sb.st_mtime);
+    get_user_details(&data, sb.st_uid, sb.st_gid);
+    get_size(&data, sb.st_size);
+    get_links(&data, sb.st_nlink);
+
+    print_file(&data);
 }
 
 void	get_folder_info(t_args *meta)
